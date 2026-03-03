1c9830231275d097d54f11ae517f3c8a2e57f8dd
momi42 <mfalahj.gmail.com>
2026-03-04 00:01:29 +0100

    fino ad atoi

--- 2_do_op.c
+++ 2_do_op.c
@@ -26,3 +26,26 @@ $> ./do_op "1" "+" "-43" | cat -e
 -42$
 $> ./do_op | cat -e
 $ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int ac, char *av[])
+{
+  if (ac == 4)
+  {
+    if (av[2][0] == '+')
+      printf("%d", atoi(av[1]) + atoi(av[3]));
+    else if (av[2][0] == '-')
+      printf("%d", atoi(av[1]) - atoi(av[3]));
+    else if (av[2][0] == '*')
+      printf("%d", atoi(av[1]) * atoi(av[3]));
+    else if (av[2][0] == '/')
+      printf("%d", atoi(av[1]) / atoi(av[3]));
+    else if (av[2][0] == '%')
+      printf("%d", atoi(av[1]) % atoi(av[3]));
+  }
+  printf("\n");
+  return (0);
+}
